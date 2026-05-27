6dbc98554a1f7d2a3c200089404688abc5876025
mahi160 <omarsifat288@gmail.com>
2026-05-27 12:28:51 +0600

    fix(nvim): lazy=true, remove duplicate TS LSP and smear-cursor, drop lorem/better-escape/markdown-preview, fix duplicate italic option

--- dotfiles/nvim/.config/nvim/lua/plugins/misc.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/misc.lua
@@ -1,26 +1,14 @@
 return {
+  -- Disable markdown-preview.nvim (62MB Node.js app, superseded by live-preview.nvim)
+  { "iamcco/markdown-preview.nvim", enabled = false },
+
   {
     "brianhuster/live-preview.nvim",
+    cmd = { "LivePreview" }, -- lazy: only load when the command is used
   },
+
   {
     "wakatime/vim-wakatime",
-  },
-  {
-    "derektata/lorem.nvim",
-    config = function()
-      require("lorem").opts({
-        sentence_length = "mixed",
-        format_defaults = {},
-        comma_chance = 0.3,
-        max_commas = 2,
-        debounce_ms = 200,
-      })
-    end,
-  },
-  {
-    "max397574/better-escape.nvim",
-    config = function()
-      require("better_escape").setup()
-    end,
+    event = "VeryLazy", -- defer past startup; tracks coding time after nvim is idle
   },
 }
